add9009ef515e657fde116ab57be949cb4c8b905
Kai Wong <kai.wong.hi@gmail.com>
2023-04-16 22:29:21 -0400

    Toggle on/off Kasa plugs.

---
+++ index.js
@@ -0,0 +1,70 @@
+const { Gpio } = require( 'onoff' );
+const RPiGPIOButtons = require('rpi-gpio-buttons');
+const util = require('util');
+
+const exec = util.promisify(require('child_process').exec);
+
+const LIGHTS_ALIASES = ['pendant', 'desk', 'counter'];
+
+let isLightsOn = false;
+
+const buttons = new RPiGPIOButtons({
+  pins: [17],
+  usePullUp: false,
+});
+
+buttons.on('button_event', (type) => {
+  if (type === 'clicked' || type === 'released') {
+    onButtonClick();
+  }
+});
+
+buttons
+  .init()
+  .catch(error => {
+    console.log('ERROR', error.stack);
+    process.exit(1);
+  });
+
+async function onButtonClick() {
+  updateProcessingLEDState(true);
+  try {
+    await toggleLights();
+  } catch (e) {
+    console.log(e);
+  } finally {
+    updateProcessingLEDState(false);
+  }
+}
+
+async function toggleLights() {
+  await Promise.all(LIGHTS_ALIASES.map(alias => {
+    return execKasa(alias, isLightsOn ? 'off' : 'on');
+  }));
+  isLightsOn = !isLightsOn;
+}
+
+async function execKasa(deviceAlias, state) {
+  const { stdout, stderr } = await exec(`kasa --type plug --alias ${deviceAlias} ${state}`);
+  if (stderr) {
+    console.log('stderr:', stderr);
+  } else {
+    console.log('stdout:', stdout);
+  }
+}
+
+function updateProcessingLEDState(on) {
+  const ledOut = new Gpio('4', 'out');
+  ledOut.writeSync( on ? 1 : 0 );
+}
+
+console.log('Initialized');
+
+// Status LED
+const ledOut = new Gpio('27', 'out');
+setInterval(() => {
+  ledOut.writeSync(1);
+  setTimeout(() => {
+    ledOut.writeSync(0);
+  }, 500);
+}, 5000);
